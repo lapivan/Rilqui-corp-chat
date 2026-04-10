da80190014842e2872b5fa0ddd0fb1b85b0d59bb
lapivan <lapunoujohn@gmail.com>
2026-04-10 20:03:05 +0300

    feat(domain): add IUnitOfWork interface

---
+++ src/backend/RilquiChat.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace RilquiChat.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IChatRepository Chats { get; }
+    IMessageRepository Messages { get; }
+    IUserRepository Users { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task DeleteDataBaseAsync();
+    Task CreateDataBaseAsync();
+}
