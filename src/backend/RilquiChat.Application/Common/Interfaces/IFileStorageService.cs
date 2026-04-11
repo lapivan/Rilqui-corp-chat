f29fe9f8e74fa05668eeee516234d0bc94c665bd
lapivan <lapunoujohn@gmail.com>
2026-04-11 21:32:22 +0300

    feat(application): add external service interfaces and DI

---
+++ src/backend/RilquiChat.Application/Common/Interfaces/IFileStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace RilquiChat.Application.Common.Interfaces;
+
+public interface IFileStorageService
+{
+    Task<string> UploadFileAsync(Stream fileStream, string fileName, CancellationToken ct = default);
+    Task DeleteFileAsync(string fileUrl, CancellationToken ct = default);
+}
---
+++ src/backend/RilquiChat.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using RilquiChat.Application.Common.Behaviors;
+
+namespace RilquiChat.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(TransactionBehavior<,>));
+        });
+        
+        services.AddValidatorsFromAssembly(assembly);
+        var config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(assembly);
+        services.AddSingleton(config);
+        services.AddScoped<IMapper, ServiceMapper>();
+
+        return services;
+    }
+}
