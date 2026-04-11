a4e2d7f64029f24b92702b0cd41eced999d0e93a
lapivan <lapunoujohn@gmail.com>
2026-04-11 19:30:26 +0300

    feat(infrastructure): add di for WebApi

---
+++ src/backend/RilquiChat.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RilquiChat.Infrastructure.Data;
+using RilquiChat.Infrastructure.Repositories;
+
+namespace RilquiChat.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IChatRepository, ChatRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
